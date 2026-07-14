0cd226608bde7af28271cd397d66453ec1e53c95
het8023 <858776802@qq.com>
2026-07-14 13:44:38 +0800

    feat：更新het3d依赖版本

---
+++ assets/oitFinalSimpleBlend.fragment-BLZR-7Ih-DYfAWEIv.js
@@ -0,0 +1,2 @@
+import{I as o}from"./index-BrdItevI.js";const r="oitFinalSimpleBlendPixelShader",e=`precision highp float;uniform sampler2D uFrontColor;void main() {ivec2 fragCoord=ivec2(gl_FragCoord.xy);vec4 frontColor=texelFetch(uFrontColor,fragCoord,0);glFragColor=frontColor;}
+`;o.ShadersStore[r]||(o.ShadersStore[r]=e);
